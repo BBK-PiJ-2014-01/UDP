4c87be4bdd72f6b049e2b55137649e106edbb9b1
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-04-12 17:11:30 +0100

    Refactoring the test checking the toByteArray and fromByteArray methods

--- Tests/TestFileFactory.java
+++ Tests/TestFileFactory.java
@@ -9,13 +9,15 @@ import java.io.File;
 public class TestFileFactory {
 
     @Test
-    public void tests_fileIsSplitInChunks() {
+    public void tests_audioFileIsConvertedToByteArray_convertedBackToFileThatCanBePlayed() {
         AudioClient ac = new AudioClientImpl();
         File audioFile = new File("./firetrucks.wav");
+        // Converting an audioFile into a byte array
         byte[] fileChunks = FileFactory.toByteArray(audioFile);
+        // Converting the byte array back to a .wav file
         File newFile = new File("./newfile.wav");
-
         newFile = FileFactory.fromByteArray(fileChunks, newFile.getPath());
+        // Playing the newly created file
         ac.playAudio(newFile);
     }
 }
