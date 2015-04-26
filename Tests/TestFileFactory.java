1125ac5b26dafe3d9377bd405a3246c2b90c9153
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-04-26 12:13:27 +0100

    Tests for FileFactory interface completed. Added test for ConcatenateByteArrays() method.

--- Tests/TestFileFactory.java
+++ Tests/TestFileFactory.java
@@ -3,6 +3,7 @@
  */
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 
@@ -20,4 +21,18 @@ public class TestFileFactory {
         // Playing the newly created file
         ac.playAudio(newFile);
     }
+
+    @Test
+    public void tests_concatenateBytesArray_ReturnsCorrectConcatenatedArray() {
+        String message1 = "Pierre";
+        byte[] byteArray1 = message1.getBytes();
+        String message2 = "Meyer";
+        byte[] byteArray2 = message2.getBytes();
+
+        String expectedMessage = "PierreMeyer";
+
+        byte[] actualByteArray = FileFactory.concatenateByteArrays(byteArray1, byteArray2);
+        assertEquals("Incorrect byte array generated", expectedMessage, new String(actualByteArray));
+    }
+
 }
