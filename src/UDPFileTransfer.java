e742462be5af9bc04a584b11b9b0e96e28c96d01
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-04-26 22:32:08 +0100

    Added comments in the code Added Error flag with warning message, if a data packet is sent as many times as the maximum number attempts allowed

--- src/UDPFileTransfer.java
+++ src/UDPFileTransfer.java
@@ -21,7 +21,10 @@ public interface UDPFileTransfer {
 
     /**
      * Sends a file in chunks of 32768 bytes to a receiver on "Local Host" - Port 5000, using the UDP protocol
-     * To make sure the receiver receives all data packets and in order, the client:
+     *
+     * To enhance the quality of the UDP transmission(e.g. receiver receives all data packets and in order), the client:
+     * - includes a 4 bytes header in the data packet, specifying the length of the data included in the packet
+     * (data length is usually different in the last data packet transferred)
      * - waits for receipt acknowledgement from the receiver after each data packet
      * - attempts a maximum of three times sending the data packet again if no acknowledgement is received
      *
@@ -31,22 +34,28 @@ public interface UDPFileTransfer {
 
         try (DatagramSocket sendingSocket = new DatagramSocket()) {
             InetAddress IPAddress = InetAddress.getByName("localhost");
+            // Converts audio file into a byte array
             byte[] dataToTransfer = FileFactory.toByteArray(audioFile);
             byte[] interimPacket;
             byte[] sendData;
             byte[] receiveData = new byte[12];
-            boolean finished = false;
+            boolean finished = false;   // Flag = true when the transfer is completed
+            boolean errorFlag = false;  // Flag = true when the transfer may not have been successful
             int i=0;
             do {
                 if(((i+1)*PACKET_SIZE) <= dataToTransfer.length)
                     interimPacket = Arrays.copyOfRange(dataToTransfer, i*PACKET_SIZE, (i+1)*PACKET_SIZE);
                 else
                     interimPacket = Arrays.copyOfRange(dataToTransfer, i*PACKET_SIZE, dataToTransfer.length);
+                // Build header with the length of the data byte array
                 byte[] interimPacketLength = ByteBuffer.allocate(4).putInt(interimPacket.length).array();
                 System.out.println("Packet length: "+interimPacket.length);
+                // Concatenate header & data byte arrays
                 sendData = FileFactory.concatenateByteArrays(interimPacketLength,interimPacket);
                 String receiverReply = null;
                 int attempt = 0;
+                // For each data packet, waits for 100ms.
+                // seeks receiver acknowledgement. Attempts sending 3 times maximum if no acknowledgement.
                 do {
                     DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, PORT_NUMBER_UDP);
                     sendingSocket.send(sendPacket);
@@ -60,19 +69,26 @@ public interface UDPFileTransfer {
                     sendingSocket.receive(receivePacket);
                     receiverReply = new String(receivePacket.getData());
                     System.out.println("FROM RECEIVER:" + receiverReply);
+                    if (attempt == MAX_SENDING_ATTEMPT)
+                        errorFlag = true;
                 } while(!receiverReply.equals("ACKNOWLEDGED")||(attempt == MAX_SENDING_ATTEMPT));
                 if ((i+1)*PACKET_SIZE > dataToTransfer.length)
                     finished = true;
                 i++;
             }while(!finished);
 
+            // Sends message to receiver that the transfer is completed
             String message = "COMPLETED";
             byte[] clientMessage = message.getBytes();
             byte[] interimPacketLength = ByteBuffer.allocate(4).putInt(clientMessage.length).array();
             sendData = FileFactory.concatenateByteArrays(interimPacketLength,clientMessage);
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, PORT_NUMBER_UDP);
             sendingSocket.send(sendPacket);
-            System.out.println("File uploaded!");
+            // Warning sent if the maximum number of attempts was made to send the same packet
+            if (errorFlag)
+                System.out.println("File uploaded but latency issues may have corrupted it");
+            else
+                System.out.println("File uploaded!");
 
         } catch (SocketException ex) {
             System.out.println(ex.getMessage());
@@ -85,6 +101,11 @@ public interface UDPFileTransfer {
 
     /**
      * Receives a file from a sender on Port 5000, using the UDP protocol
+     *
+     * - Extracts the data part of the received packet, based on the info included in the packet header
+     * - Acknowledges receipt of each packet received
+     * - Concatenates all data packets received into a single byte array
+     * - Converts the final version of the byte array back into an audio file
      *
      * @param filePath filePath to allocate to the received file
      * @return file received
@@ -103,10 +124,13 @@ public interface UDPFileTransfer {
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 receivingSocket.receive(receivePacket);
                 byte[] fromSender = receivePacket.getData();
+                // Identifies data size from header
                 byte[] dataPacketSize = Arrays.copyOfRange(fromSender, 0, 4);
                 int dataSize = ByteBuffer.wrap(dataPacketSize).getInt();
+                // Separate header from data
                 senderMessage = Arrays.copyOfRange(fromSender,4,dataSize+4);
                 fromSenderTrimmed= new String(senderMessage);
+                // Concatenate all data packets received so far
                 if (!fromSenderTrimmed.equals("COMPLETED")) {
                     if ((dataToReceive != null)) {
                         byte[] interim = dataToReceive;
@@ -116,12 +140,14 @@ public interface UDPFileTransfer {
                 }
                 InetAddress IPAddress = receivePacket.getAddress();
                 int port = receivePacket.getPort();
+                // Confirms acknowledgement of packet received
                 String receiverMessage = "ACKNOWLEDGED";
                 sendData = receiverMessage.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
                 receivingSocket.send(sendPacket);
             } while (!fromSenderTrimmed.equals("COMPLETED"));
             System.out.println("File uploaded: Size: "+dataToReceive.length);
+            // Converts data byte array back into a file
             receivedFile = FileFactory.fromByteArray(dataToReceive,filePath);
         }catch (SocketException ex) {
             System.out.println(ex.getMessage());
